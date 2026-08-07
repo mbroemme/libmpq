f5745cbf55ecf6878b7fc903e39d25723ec6048c
Maik Broemme <mbroemme@libmpq.org>
2026-08-07 14:07:35 +0200

    chore: relicense libmpq under LGPL-2.1-or-later
    Update source, binding, tool, documentation, pkg-config, and Debian
    license notices from GPL-2.0-or-later to LGPL-2.1-or-later after the
    recorded contributor approvals in issue #21.
    
    Add the LGPL-2.1 license text and a relicensing record documenting the
    approval links, prior GPL release status, and preserved third-party
    provenance notices.
    
    Include the new licensing files in release archives and fix distcheck
    coverage by distributing the tools directory and private headers needed
    to build from the generated tarball.

--- libmpq/mpq.h
+++ libmpq/mpq.h
@@ -10,19 +10,18 @@
  *  Ladislav Zezula <ladik@zezula.net>
  *  Marko Friedemann <marko.friedemann@bmx-chemnitz.de>
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
+ *  This file is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation; either version 2.1 of the License, or
  *  (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
+ *  This file is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ *  GNU Lesser General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this file; if not, see <https://www.gnu.org/licenses/>.
  */
 
 #ifndef _MPQ_H
