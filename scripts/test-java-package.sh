f0fad48abf855feb10a7a7e72fb52330b5483781
Maik Broemme <mbroemme@libmpq.org>
2026-08-25 19:21:04 +0200

    chore(scripts): add license headers
    Add the standard LGPL-2.1-or-later copyright header to every script under
    scripts/ and keep the script validation checks passing.

--- scripts/test-java-package.sh
+++ scripts/test-java-package.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# Copyright (c) 2026 Maik Broemme <mbroemme@libmpq.org>
+#
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation; either version 2.1 of the License, or
+# (at your option) any later version.
+
 set -euo pipefail
 
 if (($# < 3 || $# > 4)); then
